ebef502f04fde3305786568f40020358a34b6041
John Mair <jrmair@gmail.com>
2021-05-15 23:26:01 +0200

    Debug builds by default

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p out
 pushd out
-cmake .. -DCMAKE_TOOLCHAIN_FILE=/Users/john/code/vcpkg/scripts/buildsystems/vcpkg.cmake 
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=/Users/john/code/vcpkg/scripts/buildsystems/vcpkg.cmake
 make
 
 popd
